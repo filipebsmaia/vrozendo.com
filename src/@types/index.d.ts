1880006888abf8afe0a2533a96423ec76b6677c5
Filipe Maia <filipebsmaia@gmail.com>
2022-08-25 23:26:02 -0300

    feat: added gtm

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,1 +1,8 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare module '*.png';
+declare global {
+  interface Window {
+    dataLayer: Record<string, any>[];
+  }
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,19 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600&display=swap" rel="stylesheet" />
 
+          {/* Google tag (gtag.js) */}
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', 'G-SZP75F8JCK');
+              `
+            }}
+          />
         </Head>
         <body>
           <Main />
