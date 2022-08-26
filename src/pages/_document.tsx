2c1775d15282ce1b28031c7755093c6b90d469b0
Filipe Maia <filipebsmaia@gmail.com>
2022-08-25 23:31:51 -0300

    feat: changed script for gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import Document, {
   Html, Head, Main, NextScript
 } from 'next/document';
+import Script from 'next/script';
 
 export default class MyDocument extends Document {
   render(): JSX.Element {
@@ -13,16 +14,21 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600&display=swap" rel="stylesheet" />
 
           {/* Google tag (gtag.js) */}
-          <script
-            // eslint-disable-next-line react/no-danger
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-SZP75F8JCK"
+          />
+          <Script
+            strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer', 'G-SZP75F8JCK');
-              `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SZP75F8JCK', {
+              page_path: window.location.pathname,
+            });
+          `
             }}
           />
         </Head>
